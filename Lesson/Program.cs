bbdce61b6c059c010b13cae01cf60873cf2136d6
Пользователь <@DESKTOP-0IR19GQ>
2023-04-18 23:22:10 +0300

    Добавьте файлы проекта.

---
+++ Lesson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Комментарий //
+
+
+namespace Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int a = 9;
+
+            int b;
+
+            b = 4;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
